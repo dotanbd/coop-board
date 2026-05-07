5379cac0bf9303236f57615ace98e054f8c33fa4
dotanbd <107813135+dotanbd@users.noreply.github.com>
2026-05-07 13:32:00 +0300

    connected alembic to the main db

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
+from main import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +20,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
+DB_FILE_NAME = os.getenv("DB_FILE", "teaspoon_v1.db")
+config.set_main_option("sqlalchemy.url", f"sqlite:///./data/{DB_FILE_NAME}")
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
